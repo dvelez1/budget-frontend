dfc6138d36971367053801efef6ec31df1b72003
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-01-29 14:23:52 -0400

    Add NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <template>
   <!-- <Budget /> -->
-  <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/budget">Budget</router-link> 
-  </div>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Budget</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link active" aria-current="page"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link active" aria-current="page"
+              >About</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/budget" class="nav-link active" aria-current="page"
+              >Budget</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
   <router-view />
 </template>
 
@@ -12,13 +44,12 @@
 // import Budget from './components/Budget.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // Budget,
-  }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
